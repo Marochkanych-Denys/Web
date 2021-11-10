de9b316df99ce6dbb979976c3aea2615f1766e87
Denys Marochkanych <35735787+Marochkanych-Denys@users.noreply.github.com>
2021-11-10 23:56:53 +0200

    Add files via upload

---
+++ predictions_site/vanga_app/templates/Script.js
@@ -0,0 +1,6 @@
+function start(){
+    element = document.getElementById("list")
+    for(true){
+        element.innerHTML ='  (＾ｖ＾)'+'<br>'+'    ||  //'+'<br>'+' //(||)//'+'<br>'+' \\(||)'+'<br>'+'   //\\'+'<br>'+'  //  \\'+'<br>'+' //    \\'+'<br>'
+    }
+}
